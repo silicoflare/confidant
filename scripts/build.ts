8acfc09ea5ef3f04bc89a45eab6fc2d85378f293
Suraj B M <silicoflare@gmail.com>
2024-09-30 23:13:53 +0530

    fix: update README and minify binaries during build

--- scripts/build.ts
+++ scripts/build.ts
@@ -5,7 +5,7 @@ const { version: ver } = data;
 
 await $`
 rm -rf ./dist
-bun build --compile --target=bun-linux-x64 ./confidant.ts --outfile ./dist/confidant_${ver}_linux_x64
-bun build --compile --target=bun-windows-x64 ./confidant.ts --outfile ./dist/confidant_${ver}_win_x64.exe
-bun build --compile --target=bun-darwin-arm64 ./confidant.ts --outfile ./dist/confidant_${ver}_darwin_arm64
+bun build --compile --minify --target=bun-linux-x64 ./confidant.ts --outfile ./dist/confidant_${ver}_linux_x64
+bun build --compile --minify --target=bun-windows-x64 ./confidant.ts --outfile ./dist/confidant_${ver}_win_x64.exe
+bun build --compile --minify --target=bun-darwin-arm64 ./confidant.ts --outfile ./dist/confidant_${ver}_darwin_arm64
 `;
